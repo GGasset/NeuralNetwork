a08438fa7d818cbf2d81d1b62f30b57cc5acb138
Germán <GassetGerman@gmail.com>
2023-07-27 14:21:37 +0200

    probably finished INeuron.h

--- INeuron.h
+++ INeuron.h
@@ -1,13 +1,22 @@
+#include "IConnections.h"
+
 #pragma once
 class INeuron
 {
-	/// <summary>
-	/// Input layer not included as its activations are the input
-	/// </summary>
-	int layer_i;
+protected:
+	IConnections connections;
 
-	virtual double* Execute();
+public:
+	virtual double* ExecuteStore(double**);
+
+	virtual double Execute(double**);
 
 	virtual double GetOutput(double*);
+
+	virtual double* GetGradients(double neuron_cost, double** networkCosts, double** network_activations, double* neuronActivations);
+
+	virtual void SubtractGradients(double* neuronGradients);
+
+	virtual void DeleteMemory();
 };
 
