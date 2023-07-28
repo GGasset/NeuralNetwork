bd1d4d55ce00316da896f048a42268d20b24b5bb
Germán <GassetGerman@gmail.com>
2023-07-28 14:28:01 +0200

    Added constructor Added ExecuteStore()

--- Neuron.h
+++ Neuron.h
@@ -1,7 +1,29 @@
 #include "INeuron.h"
+# include "ActivationFunctions.h"
 
 #pragma once
-class Neuron: INeuron
+class Neuron : public INeuron
 {
+	ActivationFunctions::ActivationFunction activation_function;
+
+	Neuron(IConnections* connections, double bias, ActivationFunctions::ActivationFunction activation_function)
+	{
+		this->connections = connections;
+		this->bias = bias;
+		this->activation_function = activation_function;
+	}
+
+	double* INeuron::ExecuteStore(double** networkActivations)
+	{
+		double* output = new double[3];
+		double linear_function = connections->LinearFunction(networkActivations);
+		output[0] = linear_function;
+
+		linear_function += bias;
+		output[1] = linear_function;
+
+		output[2] = ActivationFunctions::Activate(linear_function, this->activation_function);
+		return output;
+	}
 };
 
