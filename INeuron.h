7205a1bf891957430f1531bc8bdb19cbdd6f6f71
Germán <GassetGerman@gmail.com>
2023-07-28 12:19:33 +0200

    written parameter names | improved design

--- INeuron.h
+++ INeuron.h
@@ -7,17 +7,22 @@ protected:
 	IConnections* connections;
 
 public:
-	virtual double* ExecuteStore(double**) = 0;
+	virtual double* ExecuteStore(double** networkActivations) = 0;
 
-	virtual double Execute(double**) = 0;
+	virtual double Execute(double** network_activations) = 0;
 
-	virtual double GetOutput(double*) = 0;
+	virtual double GetOutput(double* network_activations) = 0;
 
 protected:
 	virtual double* GetGradients(double neuron_cost, double** networkCosts, double** network_activations) = 0;
 
 public:
-	virtual double** GetGradients(double*** network_costs, double*** network_activations) = 0;
+	/// <param name="network_gradients_over_t">
+	///		Fourth dimension: t.
+	///		Third-second dimension layer-neuron.
+	///		First dimension: gradients calculated at GetGradients.
+	/// </param>
+	virtual void GetGradients(size_t calculated_steps, double**** output, double*** network_costs, double*** network_activations) = 0;
 
 	virtual void SubtractGradients(double* neuronGradients) = 0;
 
