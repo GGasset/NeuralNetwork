1a7a46a859e12985086858ec10487ab922a5c9c8
Germán <GassetGerman@gmail.com>
2023-07-28 16:54:32 +0200

    added derivatives of activation functions

--- Derivatives.h
+++ Derivatives.h
@@ -4,6 +4,51 @@
 static class Derivatives
 {
 public:
+	// Activation Functions derivatives
+
+	static double DerivativeOf(double x, ActivationFunctions::ActivationFunction ActivationType)
+	{
+		switch (ActivationType)
+		{
+		case ActivationFunctions::RELU:
+			return RELUDerivative(x);
+		case ActivationFunctions::Sigmoid:
+			return SigmoidDerivative(x);
+		case ActivationFunctions::Tanh:
+			return TanhDerivative(x);
+		case ActivationFunctions::None:
+			return 1;
+		default:
+			return NULL;
+		}
+	}
+
+	static double RELUDerivative(double x)
+	{
+		return x * (x >= 0);
+	}
+
+	static double SigmoidDerivative(double x)
+	{
+		return ActivationFunctions::SigmoidActivation(x) * (1 - ActivationFunctions::SigmoidActivation(x));
+	}
+
+	static double TanhDerivative(double x)
+	{
+		double bottomDerivative = expDerivative(-2 * x);
+		return -DivisionDerivative(2, 1 + exp(-2 * x), 0, bottomDerivative);
+	}
+
+	static double DivisionDerivative(double a, double b, double Da, double Db)
+	{
+		return (Da * b - Db * a) / (b * b);
+	}
+
+	static double expDerivative(double x)
+	{
+		return exp(x);
+	}
+
 	//Cost derivatives
 
 	static double* DerivativeOf(size_t networkOutputLength, double* networkOutput, double* Y, Cost::CostFunction costFunction)
