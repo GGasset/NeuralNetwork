f3a3f996528cad5cca3e2bf4d38f1d3ad7b9d138
Germán <GassetGerman@gmail.com>
2023-07-28 10:36:55 +0200

    probably finished IConnections

--- IConnections.h
+++ IConnections.h
@@ -1,5 +1,43 @@
+#include <iostream>
+
 #pragma once
 class IConnections
 {
+protected:
+	/// <summary>
+	/// Input layer not included as its activations are the input
+	/// </summary>
+	size_t layer_i;
+	size_t neuron_i;
+	size_t weight_count;
+	double* weights;
+	
+public:
+
+	size_t GetLayerI()
+	{
+		return layer_i;
+	}
+
+	size_t GetNeuronI()
+	{
+		return neuron_i;
+	}
+
+	size_t GetWeightCount()
+	{
+		return weight_count;
+	}
+
+	void Free()
+	{
+		if (weights)
+			free(weights);
+	}
+
+	virtual double LinearFunction(double** network_activations);
+	virtual double* GetGradients(size_t output_write_start, double** network_activations, double** network_costs, double linear_function_gradient);
+	virtual void SubtractGradients(double* gradients, size_t input_read_start);
+	virtual void SubtractGradients(double** gradients, size_t input_read_start);
 };
 
