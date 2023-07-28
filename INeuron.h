04f3909f7741de4ffd461d544897a749fd05fb48
Germán <GassetGerman@gmail.com>
2023-07-28 16:56:56 +0200

    Fixed bugs

--- INeuron.h
+++ INeuron.h
@@ -15,7 +15,7 @@ public:
 	virtual double GetOutput(double* execute_store_output) = 0;
 
 public:
-	virtual double* GetGradients(double neuron_cost, double** network_costs, double** network_activations) = 0;
+	virtual double* GetGradients(double* execution_results, double neuron_cost, double** network_costs, double** network_activations) = 0;
 
 public:
 	/// <param name="network_gradients_over_t">
@@ -23,7 +23,7 @@ public:
 	///		Third-second dimension layer-neuron.
 	///		First dimension: gradients calculated at GetGradients.
 	/// </param>
-	virtual void GetGradients(size_t calculated_steps, double**** output, double*** network_costs, double*** network_activations) = 0;
+	virtual void GetGradients(double**** execution_results, size_t calculated_steps, double**** output, double*** network_costs, double*** network_activations) = 0;
 
 	virtual void SubtractGradients(double* neuronGradients) = 0;
 
