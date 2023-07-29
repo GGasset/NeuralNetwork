aa49cb51cc884f2f37c93b6cd6c10058ba358367
Germán <GassetGerman@gmail.com>
2023-07-29 12:15:53 +0200

    fixed bug

--- ILayer.h
+++ ILayer.h
@@ -9,13 +9,13 @@ class ILayer
 protected:
 	size_t layer_length = -1;
 public:
-	INeuron* neurons = 0;
+	INeuron** neurons = 0;
 
 	void free()
 	{
 		for (size_t i = 0; i < layer_length; i++)
 		{
-			neurons[i].Free();
+			neurons[i]->Free();
 			delete (neurons + i);
 		}
 		delete[] neurons;
