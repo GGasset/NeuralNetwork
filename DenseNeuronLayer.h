677943875a75d15fd5a9725087975fdcb1581156
Germán <GassetGerman@gmail.com>
2023-07-31 16:48:58 +0200

    Fixed bug

--- DenseNeuronLayer.h
+++ DenseNeuronLayer.h
@@ -9,7 +9,7 @@ public:
 	DenseNeuronLayer(size_t* network_shape, size_t layer_i, ActivationFunctions::ActivationFunction activation_function)
 	{
 		this->layer_length = network_shape[layer_i];
-		this->neurons = new INeuron*[layer_length];
+ 		this->neurons = new INeuron*[layer_length];
 		for (size_t i = 0; i < layer_length; i++)
 		{
 			DenseConnections* connections = new DenseConnections(layer_i, i, network_shape);
