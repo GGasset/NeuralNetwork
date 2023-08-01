35cd9e9ee635701b9b142376dd75c72512558ff7
Germán <GassetGerman@gmail.com>
2023-08-01 13:35:25 +0200

    finished linear function

--- DenseConnections.h
+++ DenseConnections.h
@@ -26,10 +26,12 @@ public:
 
 	double IConnections::LinearFunction(double* network_activations)
 	{
+		double linear_function = 0;
 		for (size_t i = 0; i < previous_layer_length; i++)
 		{
-
+			linear_function += network_activations[previous_layer_start_i + i] * weights[i];
 		}
+		return linear_function;
 	}
 
 	void IConnections::CalculateGradients(double* gradients, double* neuron_activations, double* costs)
