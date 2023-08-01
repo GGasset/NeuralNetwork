53c6d3cd73a0a7343f3f4921c0ce4d5efa24a0ee
Germán <GassetGerman@gmail.com>
2023-08-01 17:29:33 +0200

    Updated per "I'm no three star programmer"

--- INeuron.h
+++ INeuron.h
@@ -6,21 +6,21 @@ class INeuron
 protected:
 	size_t neuron_i = -1;
 
-	/// <summary>
-	/// Execution results must have the same values per neuron as gradients per neuron
-	/// </summary>
-	size_t self_execution_results_start_i = -1;
-	size_t self_execution_results_length = -1;
-
 	double bias = 0;
 
 public:
+	/// <summary>
+	/// Execution results must have the same values per neuron as gradients per neuron.
+	/// Value will be automatically asigned by NN
+	/// </summary>
+	size_t self_execution_results_start_i = -1;
+	/// <summary>
+	/// Value will be automatically asigned by NN
+	/// </summary>
+	size_t self_execution_results_length = -1;
+
 	IConnections* connections = 0;
 
-	size_t GetExecutionResultsLength()
-	{
-		return self_execution_results_length;
-	}
 
 	virtual void ExecuteStore(double* activations, double* execution_results) = 0;
 	virtual void Execute(double* activations) = 0;
