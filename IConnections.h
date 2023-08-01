5d910ae813a4a6806bcca093fd6365eb31ac9f0c
Germán <GassetGerman@gmail.com>
2023-08-01 11:08:09 +0200

    Finished interface

--- IConnections.h
+++ IConnections.h
@@ -14,28 +14,13 @@ protected:
 	/// <summary>
 	/// Execution results must have the same values per neuron as gradients per neuron
 	/// </summary>
-	size_t self_execution_results_start_i = -1;
-	size_t self_execution_results_length = -1;
-	size_t neuron_gradients_written_value_count = -1;
+	size_t self_gradients_start_i = -1;
+	size_t self_gradients_length = -1;
+	size_t neuron_written_gradient_count = -1;
 	size_t weight_count = -1;
 	double* weights = 0;
 	
 public:
-
-	size_t GetNeuronI()
-	{
-		return neuron_i;
-	}
-
-	size_t GetExecutionResultsStart()
-	{
-		return self_execution_results_start_i;
-	}
-
-	size_t GetExecutionResultsLength()
-	{
-		return self_execution_results_length;
-	}
 
 	void Free()
 	{
@@ -45,7 +30,7 @@ public:
 	}
 
 	virtual double LinearFunction(double* network_activations) = 0;
-	virtual void CalculateGradients(double* gradients, double* execution_results) = 0;
+	virtual void CalculateGradients(double* gradients, double* neuron_activations) = 0;
 	virtual void SubtractGradients(double* gradients) = 0;
 	virtual void SubtractGradients(double* gradients) = 0;
 };
