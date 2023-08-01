5c204157dfa20da92517f741be102741602e1ea0
Germán <GassetGerman@gmail.com>
2023-08-01 11:13:44 +0200

    finished interface

--- DenseConnections.h
+++ DenseConnections.h
@@ -10,5 +10,9 @@ class DenseConnections :
     size_t previous_layer_length = -1;
 
 public:
+    double IConnections::LinearFunction(double* network_activations) = 0;
+    void IConnections::CalculateGradients(double* gradients, double* execution_results) = 0;
+    void IConnections::SubtractGradients(double* gradients) = 0;
+    void IConnections::SubtractGradients(double* gradients) = 0;
 };
 
--- INeuron.h
+++ INeuron.h
@@ -4,37 +4,25 @@
 class INeuron
 {
 protected:
+	size_t neuron_i = -1;
+
+	/// <summary>
+	/// Execution results must have the same values per neuron as gradients per neuron
+	/// </summary>
+	size_t self_execution_results_start_i = -1;
+	size_t self_execution_results_length = -1;
+	size_t neuron_gradients_written_value_count = -1;
+
 	IConnections* connections = 0;
 	double bias = 0;
 
 public:
-	virtual double* ExecuteStore(double** networkActivations) = 0;
-
-	virtual double Execute(double** network_activations) = 0;
-
-	virtual double GetOutput(double* execute_store_output) = 0;
-
-public:
-	virtual double* GetGradients(double* execution_results, double neuron_cost, double** network_costs, double** network_activations) = 0;
-
-public:
-	/// <param name="network_gradients_over_t">
-	///		Fourth dimension: t.
-	///		Third-second dimension layer-neuron.
-	///		First dimension: gradients calculated at GetGradients.
-	/// </param>
-	/// <param name="execution_results">
-	///		Fourth dimension: t.
-	///		Third-second dimension layer-neuron.
-	///		First dimension: values calculated at ExecuteStore().
-	/// </param>
-	virtual void GetGradients(double**** execution_results, size_t calculated_steps, double**** output, double*** network_costs, double*** network_activations) = 0;
-
-	virtual void SubtractGradients(double* neuron_gradients, double learning_rate) = 0;
-
-	virtual void SubtractGradients(double**** network_gradients_over_t, size_t calculated_steps, double learning_rate) = 0;
-
 	virtual void ExecuteStore(double* activations, double* execution_results) = 0;
+	virtual void Execute(double* activations) = 0;
+	virtual void GetGradients(double* gradients, double* costs, double* execution_results) = 0;
+	virtual void GetGradients(double* gradients, double* costs, size_t t_length, size_t t_count) = 0;
+	virtual void SubtractGradients(double* gradients, double learning_rate) = 0;
+	virtual void SubtractGradients(double* gradients, double learning_rate, size_t t_length, size_t t_count) = 0;
 	virtual void DeleteMemory() = 0;
 	virtual void Free() = 0;
 };
