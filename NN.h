f4336a2f3929c922825e1ba41e6611f088d9104e
Germán <GassetGerman@gmail.com>
2023-08-02 11:21:47 +0200

    finished NN constructor

--- NN.h
+++ NN.h
@@ -1,14 +1,38 @@
 #include <list>
 #include <tuple>
 
+#include "INeuron.h"
+#include "Cost.h"
+
 #pragma once
 class NN
 {
 private:
+	INeuron** neurons = 0;
 
 public:
-	NN()
+	NN(INeuron** neurons, size_t neuron_count)
 	{
+		size_t network_execution_results_value_count = 0;
+		for (size_t i = 0; i < neuron_count; i++)
+		{
+			INeuron* current_neuron = neurons[i];
+
+			// Set
+			current_neuron->self_execution_results_start_i = network_execution_results_value_count;
+			current_neuron->connections->self_gradients_start_i = network_execution_results_value_count;
+			current_neuron->connections->network_neuron_count = neuron_count;
+
+
+			//Get
+			network_execution_results_value_count += current_neuron->GetNeuronWrittenGradientCount();
+			network_execution_results_value_count += current_neuron->connections->GetWeightCount();
+		}
+
+		for (size_t i = 0; i < neuron_count; i++)
+		{
+			neurons[i]->connections->network_execution_results_value_count = network_execution_results_value_count;
+		}
 	}
 
 private:
