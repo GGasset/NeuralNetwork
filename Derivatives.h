9572b3d2cb4ce803b9fa90035df34440ce3a97b2
Germán <GassetGerman@gmail.com>
2023-08-02 17:20:33 +0200

    Now DerivativeOf output will spit nummbers one by one

--- Derivatives.h
+++ Derivatives.h
@@ -1,4 +1,4 @@
-#include "ActivationFunctions.h"
+﻿#include "ActivationFunctions.h"
 #include "Cost.h"
 
 #pragma once
@@ -52,26 +52,16 @@ public:
 
 	//Cost derivatives
 
-	static double* DerivativeOf(size_t networkOutputLength, double* networkOutput, double* Y, Cost::CostFunction costFunction)
+	static double DerivativeOf(double Y, double Ŷ, Cost::CostFunction cost_function)
 	{
-		switch (costFunction)
+		switch (cost_function)
 		{
 		case Cost::SquaredMean:
-			return SquaredMeanDerivative(networkOutputLength, networkOutput, Y);
+			return SquaredMeanDerivative(Y, Ŷ);
+			break;
 		default:
-			return NULL;
+			break;
 		}
-	}
-
-
-	static double* SquaredMeanDerivative(size_t networkOutputLength, double* networkOutput, double* Y)
-	{
-		double* output = new double[networkOutputLength];
-		for (size_t i = 0; i < networkOutputLength; i++)
-		{
-			output[i] = SquaredMeanDerivative(networkOutput[i], Y[i]);
-		}
-		return output;
 	}
 
 	static double SquaredMeanDerivative(double neuronOutput, double Y)
