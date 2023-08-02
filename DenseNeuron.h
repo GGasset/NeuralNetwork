aad9291eb5fa11a2b108ee0ffef6809b7dc4169c
Germán <GassetGerman@gmail.com>
2023-08-02 10:48:19 +0200

    finished DenseNeuron->GetGradients Functions

--- DenseNeuron.h
+++ DenseNeuron.h
@@ -30,27 +30,30 @@ public:
 		return activation;
 	}
 	
-	void INeuron::GetGradients(double* gradients, double* costs, double* execution_results)
+	void INeuron::GetGradients(double* gradients, double* costs, double* execution_results, double* network_activations)
 	{
 		double current_cost = costs[neuron_i];
 		double linear_function = execution_results[self_execution_results_start_i];
-		gradients[self_execution_results_start_i] = current_cost * Derivatives::DerivativeOf(linear_function, activation_function);
+		double linear_function_gradient =
+			gradients[self_execution_results_start_i] = current_cost * Derivatives::DerivativeOf(linear_function, activation_function);
+		connections->CalculateGradients(gradients, network_activations, costs, linear_function_gradient);
 	}
 	
-	void INeuron::GetGradients(double* gradients, double* costs, double* execution_results, size_t t_count)
+	void INeuron::GetGradients(double* gradients, double* costs, double* execution_results, double* network_activations, size_t t_count)
 	{
+		double* linear_function_gradients = new double[t_count];
 		for (size_t t = 0; t < t_count; t++)
 		{
 			double current_cost = costs[connections->network_neuron_count * t + neuron_i];
 			size_t current_gradient_start_i = connections->network_execution_results_value_count * t + self_execution_results_start_i;
 			
-			double linear_function_gradient =
+			linear_function_gradients[t] =
 				gradients[current_gradient_start_i] = 
 					Derivatives::DerivativeOf(execution_results[current_gradient_start_i], activation_function);
-
-			connections->CalculateGradients()
-
 		}
+
+		connections->CalculateGradients(gradients, network_activations, costs, linear_function_gradients, t_count);
+		delete[] linear_function_gradients;
 	}
 	
 	void INeuron::SubtractGradients(double* gradients, double learning_rate)
