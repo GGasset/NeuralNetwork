16714f82995f048f23c8238be694b1b99a3a9421
Germán <GassetGerman@gmail.com>
2023-08-03 21:05:07 +0200

    added nn prototype

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -4,11 +4,65 @@
 #include <iostream>
 
 #include "ActivationFunctions.h"
+#include "Cost.h"
+#include "NeuronLibrary.h"
 #include "NN.h"
 #include "ValueGeneration.h"
 
 int main()
 {
+	size_t t_count = 5;
+	double* X = new double[t_count];
+	double* Y = new double[t_count];
+
+	for (size_t t = 0; t < t_count; t++)
+	{
+		X[t] = 0.6;
+		Y[t] = 0;
+	}
+
+	size_t shape_length = 2;
+	size_t* shape = new size_t[shape_length];
+	shape[0] = 1;
+	shape[1] = 1;
+	
+	size_t neuron_count = 0;
+	for (size_t i = 1; i < shape_length; i++)
+	{
+		neuron_count += shape[i];
+	}
+
+	ActivationFunctions::ActivationFunction activation_function = ActivationFunctions::ActivationFunction::Sigmoid;
+	INeuron** neurons = new INeuron * [neuron_count];
+
+	size_t neuron_i = 0;
+	size_t previous_layer_start = 0;
+	// TODO: do a switch case here using neuron_type and modularize main into a single module, saving X_length and that stuff
+	for (size_t i = 1; i < shape_length; i++)
+	{
+		size_t prev_layer_length = shape[i - 1];
+		for (size_t j = 0; j < shape[i] && (neuron_i < neuron_count); j++)
+		{
+			// Bug in previous_layer_length
+			neurons[neuron_i] = new DenseNeuron(neuron_i + shape[0], previous_layer_start, prev_layer_length, activation_function);
+			neuron_i++;
+		}
+
+		previous_layer_start += shape[i - 1];
+	}
+
+	NN* n = new NN(neurons, neuron_count, shape[0], shape[shape_length - 1]);
+	for (size_t i = 0; i < 1000; i++)
+	{
+		double* output = n->Execute(X, t_count);
+		for (size_t j = 0; j < t_count; j++)
+		{
+			std::cout << "Y: " << output[j] << " | Y hat: " << Y[j] << " | i: " << i << "\n";
+		}
+
+		double learning_rate = 1;
+		n->Supervised_batch(X, Y, learning_rate, t_count, Cost::SquaredMean);
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
