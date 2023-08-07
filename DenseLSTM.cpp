d6b21f4d39368f272f5e406f4caf3a00aa8224c6
Germán <GassetGerman@gmail.com>
2023-08-07 20:34:47 +0200

    Created base for DenseLSTM

---
+++ DenseLSTM.cpp
@@ -0,0 +1,1 @@
+#include "DenseLSTM.h"
---
+++ DenseLSTM.h
@@ -0,0 +1,15 @@
+#include "DenseConnections.h"
+#include "INeuron.h"
+
+#pragma once
+class DenseLSTM : public INeuron
+{
+private:
+	double forget_weight;
+	double store_weight;
+	double output_weight;
+
+public:
+
+};
+
