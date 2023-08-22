0ffb9381d03220a7123a586a8238864810d13dab
Germán <GassetGerman@gmail.com>
2023-08-22 16:53:23 +0200

    adapted prototype for reloading nn from disk and added dropout rate

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -15,23 +15,22 @@ int main()
 {
 	srand(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count());
 
-	size_t t_count = 1;
+	size_t t_count = 2;
 	double* X = new double[t_count];
 	double* Y = new double[t_count];
 
 	for (size_t t = 0; t < t_count; t++)
 	{
-		X[t] = 10;
-		Y[t] = .2 + .1 * t;
+		X[t] = 2;
+		Y[t] = .5 + .3 * t;
 	}
 
-	size_t shape_length = 2;
+	size_t shape_length = 4;
 	size_t* shape = new size_t[shape_length];
 	shape[0] = 1;
-	shape[1] = 1;
-	/*shape[1] = 4;
-	shape[2] = 2;
-	shape[3] = 1;*/
+	shape[1] = 20;
+	shape[2] = 10;
+	shape[3] = 1;
 	
 	size_t neuron_count = 0;
 	for (size_t i = 1; i < shape_length; i++)
@@ -41,6 +40,7 @@ int main()
 
 	ActivationFunctions::ActivationFunction activation_function = ActivationFunctions::ActivationFunction::Sigmoid;
 	INeuron** neurons = new INeuron * [neuron_count];
+	NN::NeuronTypeIdentifier* neurons_id = new NN::NeuronTypeIdentifier[neuron_count];
 
 	size_t neuron_i = 0;
 	size_t previous_layer_start = 0;
@@ -51,16 +51,40 @@ int main()
 		for (size_t j = 0; j < shape[i] && (neuron_i < neuron_count); j++)
 		{
 			neurons[neuron_i] = new DenseLSTM(neuron_i + shape[0], previous_layer_start, prev_layer_length);
+			neurons_id[neuron_i] = NN::DenseLSTMId;
 			neuron_i++;
 		}
 
 		previous_layer_start += shape[i - 1];
 	}
 
-	bool continue_training = true;
-	NN* n = new NN(neurons, neuron_count, shape[0], shape[shape_length - 1]);
+	NN* n = new NN(neurons, neuron_count, shape[0], shape[shape_length - 1], neurons_id);
+	std::cout << "Before:" << std::endl;
+	double* before_output = n->Execute(X, t_count);
+	for (size_t i = 0; i < t_count; i++)
+	{
+		std::cout << before_output[i] << std::endl;
+	}
+	delete[] before_output;
+
+	std::string path = "D:\\NN\\tests\\01";
+	n->Save(path);
+	n->free();
+	n = NN::Load(path);
+
+	std::cout << std::endl << "After being loaded from disk:" << std::endl;
+	double* after_output = n->Execute(X, t_count);
+	for (size_t i = 0; i < t_count; i++)
+	{
+		std::cout << after_output[i] << std::endl;
+	}
+	delete[] after_output;
+	std::cin.get();
+	std::cout << std::endl;
+
 	double* output = 0;
-	for (size_t i = 0; i < 10000; i++)
+	bool continue_training = true;
+	for (size_t i = 0; i < 10000 && continue_training; i++)
 	{
 		double* last_output = output;
 		output = n->Execute(X, t_count);
@@ -76,17 +100,17 @@ int main()
 			{
 				double max_output = output[j] * (output[j] > last_output[j]) + last_output[j] * (last_output[j] > output[j]);
 				double min_output = output[j] * (output[j] < last_output[j]) + last_output[j] * (last_output[j] < output[j]);
-				is_same_output = is_same_output && ((max_output - min_output) < 10E-5);
+				is_same_output = is_same_output && ((max_output - min_output) < 10E-10);
 			}
 		}
 
 		if (is_same_output)
 		{
-			continue_training = !is_same_output;
+			continue_training = false;
 		}
 
-		double learning_rate = 2;
-		n->Supervised_batch(X, Y, learning_rate, t_count, Cost::SquaredMean);
+		double learning_rate = 5;
+		n->Supervised_batch(X, Y, learning_rate, t_count, Cost::SquaredMean, 0, 0, 1, .2);
 
 
 		delete[] last_output;
@@ -95,6 +119,8 @@ int main()
 	delete n;
 
 	delete[] output;
+	delete[] X;
+	delete[] Y;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
